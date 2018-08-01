28e56d7f7dd6c3e7255b462d2d324a29df8bd4b7
Brian1011 <mutindabrian26@gmail.com>
2018-08-01 13:59:09 +0300

    Additional models and controllers

---
+++ app/non_staff.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class non_staff extends Model
+{
+    //
+}
---
+++ app/parent_child_relations.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class parent_child_relations extends Model
+{
+    //
+}
